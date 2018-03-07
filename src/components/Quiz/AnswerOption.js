5588caf8da8cf06ba817acb789dce2ff63f49ce3
jacekkowalczyk91 <kowalczykjacek91@gmail.com>
2018-03-07 20:06:44 +0100

    quiz answer components added

---
+++ src/components/Quiz/AnswerOption.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AnswerOption = (props) => {
+    return (
+        <div>
+            <li className='answerOption'>
+                <input
+                    type='radio'
+                    className='radioCustomButton'
+                    name='radioGroup'
+                    checked={props.answerType === props.answer}
+                    id={props.answerType}
+                    value={props.answerTypes}
+                    disabled={props.answer}
+                    onChange={props.onAnswerSelected}
+                />
+                <label className='radioCustomLabel' htmlFor={props.answerType}>
+                    {props.answerContent}
+                </label>
+            </li>
+        </div>
+    )
+}
+
+AnswerOption.propTypes = {
+    answerType: PropTypes.string.isRequired,
+    answerContent: PropTypes.string.isRequired,
+    answer: PropTypes.string.isRequired,
+    onAnswerSelected: PropTypes.func.isRequired
+};
+
+export default AnswerOption
